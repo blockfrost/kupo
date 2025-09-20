4063b66f40be723ce7ea6d45d1a2331e0911f858
Michael Bishop <cleverca22@gmail.com>
2025-09-20 02:55:05 -0300

    set keep-alive flag on socket

--- modules/websockets-json/src/Network/WebSockets/Tls.hs
+++ modules/websockets-json/src/Network/WebSockets/Tls.hs
@@ -17,11 +17,34 @@ import Data.List
 import Data.Maybe
     ( fromMaybe
     )
+import           System.Timeout                (timeout)
+import           Control.Exception             (bracket, throwIO)
 
 import qualified Data.ByteString as BS
 import qualified Network.Connection as Network
+import qualified Network.Socket                as S
 import qualified Network.WebSockets as WS
 import qualified Wuss as WSS
+
+-- needed until https://hackage.haskell.org/package/websockets can set keep-alive itself
+runKeepAliveClient :: String -> Int -> String -> WS.ClientApp a -> IO a
+runKeepAliveClient host port path app = do
+  let
+    opts = WS.defaultConnectionOptions
+    hints    = S.defaultHints { S.addrSocketType = S.Stream }
+    fullHost = if port == 80 then host else (host ++ ":" ++ show port)
+  addr:_ <- S.getAddrInfo (Just hints) (Just host) (Just $ show port)
+  sock   <- S.socket (S.addrFamily addr) S.Stream S.defaultProtocol
+  S.setSocketOption sock S.NoDelay 1
+  S.setSocketOption sock S.KeepAlive 1
+
+  res <- bracket
+    (timeout (WS.connectionTimeout opts * 1000 * 1000) $ S.connect sock (S.addrAddress addr))
+    (const $ S.close sock) $ \maybeConnected -> case maybeConnected of
+      Nothing -> throwIO $ WS.ConnectionTimeout
+      Just () -> WS.runClientWithSocket sock fullHost path opts [] app
+
+  return res
 
 -- | A drop-in replacement for 'WS.runClient' but that also handles TLS connections.
 runClient
@@ -44,7 +67,7 @@ runClient url port =
             let
                 host = fromMaybe url (stripPrefix "ws://" url)
              in
-                WS.runClient host port "/"
+                runKeepAliveClient host port "/"
   where
     connectionGet
         :: Network.Connection
